2205153a295ffeefc5ca71af62537cb9c7153170
DylanMa <4401271@qq.com>
2022-02-23 19:51:30 +0800

    调整节点布局,增加DOM注释

--- src/index2.js
+++ src/index2.js
@@ -3,45 +3,21 @@ import ReactDOM from './react-dom';
 
 let style = {border: '2px solid skyblue', margin: '5px', borderRadius: '7px'}
 
-let element = React.createElement(
-  "div", 
-  {
-    id: "A1",
-    style: style
-  }, 
-  "A1", 
-  React.createElement(
-    "div", 
-    {
-      id: "B1",
-      style: style
-    }, 
-    "B1", 
-    React.createElement(
-      "div", 
-      {
-        id: "C1",
-        style: style
-      }, 
-      "C1"
-    ), 
-    React.createElement(
-      "div", 
-      {
-        id: "C2",
-        style: style
-      }, 
-      "C2"
-    )
-  ), 
-  React.createElement(
-    "div", 
-    {
-      id: "B2",
-        style: style
-    }, 
-    "B2"
-  )
+// let element = (
+//   <div id='A1' style={style}>A1
+//     <div id='B1' style={style}>B1
+//       <div id='C1' style={style}>C1</div>
+//       <div id='C2' style={style}>C2</div>
+//     </div>
+//     <div id='B2' style={style}>B2</div>
+//   </div>
+// )
+let element = React.createElement("div", {id: "A1", style: style}, "A1", 
+  React.createElement("div", {id: "B1", style: style}, "B1", 
+    React.createElement("div", {id: "C1", style: style}, "C1"),
+    React.createElement("div", {id: "C2", style: style}, "C2")
+  ),
+  React.createElement("div", {id: "B2", style: style}, "B2")
 );
 
 ReactDOM.render(
@@ -50,55 +26,25 @@ ReactDOM.render(
 )
 
 // 增加并修改节点
+// let element1 = (
+//   <div id='A1' style={style}>A1
+//     <div id='B1' style={style}>B1
+//       <div id='C1' style={style}>C1</div>
+//       <div id='C2-a' style={style}>C2-a</div>
+//     </div>
+//     <div id='B2-a' style={style}>B2-a</div>
+//     <div id='B3' style={style}>B3</div>
+//   </div>
+// )
 let btn1 = document.getElementById('btn1');
 btn1.addEventListener('click', () => {
-  let element1 = React.createElement(
-    "div", 
-    {
-      id: "A1-a",
-      style: style
-    }, 
-    "A1-a", 
-    React.createElement(
-      "div", 
-      {
-        id: "B1-a",
-        style: style
-      }, 
-      "B1-a", 
-      React.createElement(
-        "div", 
-        {
-          id: "C1-a",
-          style: style
-        }, 
-        "C1-a"
-      ), 
-      React.createElement(
-        "div", 
-        {
-          id: "C2-a",
-          style: style
-        }, 
-        "C2-a"
-      )
+  let element1 = React.createElement("div", {id: "A1", style: style}, "A1", 
+    React.createElement("div", {id: "B1", style: style}, "B1", 
+      React.createElement("div", {id: "C1", style: style}, "C1"), 
+      React.createElement("div", {id: "C2-a", style: style}, "C2-a")
     ), 
-    React.createElement(
-      "div", 
-      {
-        id: "B2-a",
-          style: style
-      }, 
-      "B2-a"
-    ),
-    React.createElement(
-      "div", 
-      {
-        id: "B3",
-          style: style
-      }, 
-      "B3"
-    )
+    React.createElement("div", {id: "B2-a", style: style}, "B2-a"),
+    React.createElement("div", {id: "B3", style: style}, "B3")
   );
   
   ReactDOM.render(
@@ -108,47 +54,23 @@ btn1.addEventListener('click', () => {
 });
 
 // 增加并删除节点
+// let element2 = (
+//   <div id='A1' style={style}>A1
+//     <div id='B1' style={style}>B1
+//       <div id='C1-b' style={style}>C1-b</div>
+//       <div id='C2' style={style}>C2</div>
+//     </div>
+//     <div id='B2-b' style={style}>B2-b</div>
+//   </div>
+// )
 let btn2 = document.getElementById('btn2');
 btn2.addEventListener('click', () => {
-  let element2 = React.createElement(
-    "div", 
-    {
-      id: "A1-b",
-      style: style
-    }, 
-    "A1-b", 
-    React.createElement(
-      "div", 
-      {
-        id: "B1-b",
-        style: style
-      }, 
-      "B1-b", 
-      React.createElement(
-        "div", 
-        {
-          id: "C1-b",
-          style: style
-        }, 
-        "C1-b"
-      ), 
-      React.createElement(
-        "div", 
-        {
-          id: "C2-b",
-          style: style
-        }, 
-        "C2-b"
-      )
+  let element2 = React.createElement("div", {id: "A1", style: style}, "A1", 
+    React.createElement("div", {id: "B1", style: style}, "B1", 
+      React.createElement("div", {id: "C1-b", style: style}, "C1-b"), 
+      React.createElement("div", {id: "C2", style: style}, "C2")
     ), 
-    React.createElement(
-      "div", 
-      {
-        id: "B2-b",
-          style: style
-      }, 
-      "B2-b"
-    )
+    React.createElement("div", {id: "B2-b", style: style}, "B2-b")
   );
   
   ReactDOM.render(
